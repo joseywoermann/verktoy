1ce632d4b2d6c922989328da87ecfc4aba8147f6
Josey Wörmann <jcw05@gmx.de>
2022-01-03 22:44:14 +0100

    implement mute command
    using timeouts of course

--- src/commands/mute.ts
+++ src/commands/mute.ts
@@ -1,3 +1,5 @@
+import { MessageEmbed } from "discord.js";
+import { handleError } from "../util/errorHandler.js";
 import { ChatInputCommand } from "../util/types";
 
 export const mute: ChatInputCommand = {
@@ -5,12 +7,39 @@ export const mute: ChatInputCommand = {
     description: "Mute a user for a given time",
     options: [
         {
+            name: "user",
+            description: "The user to mute",
+            type: "USER",
+            required: true,
+        },
+        {
             name: "duration",
-            description: "The dureation the user should be mutet.",
+            description: "The duration the user should be mutet.",
             type: "INTEGER",
+            required: true,
         },
     ],
     run: async (interaction) => {
-        await interaction.reply({ content: "Not implemented" });
+        if (!interaction.memberPermissions.has("MODERATE_MEMBERS")) {
+            const embed = new MessageEmbed({
+                title: "You are missing the following permission(s):",
+                description: `\`\`\`\nMODERATE_MEMBERS\`\`\``,
+                color: "RED",
+            });
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        const userId = interaction.options.get("user").user.id;
+        const duration = Number(interaction.options.get("duration").value);
+        try {
+            const member = await interaction.guild.members.fetch(userId);
+            await member.timeout(duration * 60 * 1000);
+            await interaction.reply({
+                content: `Timed out ${member} for ${duration} ${duration === 1 ? "minute" : "minutes"}.`,
+            });
+        } catch (e: unknown) {
+            await handleError(interaction, e as Error);
+        }
     },
 };
