9bc3254d27c63fca355a49d26ff84daa3826d4a8
Josey Wörmann <jcw05@gmx.de>
2022-01-09 18:11:56 +0100

    calculator command

---
+++ src/commands/calculate.ts
@@ -0,0 +1,40 @@
+import { ChatInputCommand } from "../util/types";
+import { evaluate } from "mathjs";
+
+export const calculate: ChatInputCommand = {
+    name: "calculate",
+    description: "Calculate something",
+    options: [
+        {
+            name: "expression",
+            description: "What do you want to calculate?",
+            type: "STRING",
+            required: true,
+        },
+    ],
+    run: async (interaction) => {
+        const exp = interaction.options.get("expression").value.toString();
+
+        const [success, result] = calc(exp);
+
+        if (success) {
+            await interaction.reply({ embeds: [{ title: `${result}`, description: `= ${exp}` }] });
+        } else {
+            await interaction.reply({ embeds: [{ title: `${result}` }] });
+        }
+    },
+};
+
+/**
+ * Calculates an expression. The first returned value shows whether or not the calculation was successful, the second is either the result or the error message.
+ * @param expression
+ * @returns `[success, result]`
+ */
+const calc = (expression: string): [boolean, string] => {
+    try {
+        const result: string = evaluate(expression);
+        return [true, result];
+    } catch (e: unknown) {
+        return [false, e["message"]];
+    }
+};
