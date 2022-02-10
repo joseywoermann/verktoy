7ae52910ca1d315db4d65a535a4f149e6b69e86b
Josey Wörmann <jcw05@gmx.de>
2022-02-10 22:29:11 +0100

    add 8ball & QR code commands
    remove lyrics

---
+++ src/commands/eightBall.ts
@@ -0,0 +1,49 @@
+import { ChatInputCommand } from "#util";
+import { MessageEmbed } from "discord.js";
+
+export const eightBall: ChatInputCommand = {
+    name: "8ball",
+    description: "Ask the magic 8ball",
+    options: [
+        {
+            name: "question",
+            description: "Ask a question",
+            type: "STRING",
+            required: true,
+        },
+    ],
+    run: async (interaction) => {
+        const question = interaction.options.get("question").value.toString();
+
+        const answer = allResponses[Math.floor(Math.random() * allResponses.length)];
+
+        const embed = new MessageEmbed({
+            title: `${answer}`,
+            description: `You asked: "${question}"`,
+            color: "#D329A0",
+        });
+
+        interaction.reply({ embeds: [embed] });
+    },
+};
+
+const pResponses: string[] = [
+    "Yes",
+    "Definitely",
+    "Absolutely",
+    "Most likely",
+    "Certainly",
+    "Probably",
+    "Hell yeah",
+];
+
+const nResponses: string[] = [
+    "No",
+    "Definitely not",
+    "Absolutely",
+    "Most likely not",
+    "Certainly not",
+    "Probably not",
+];
+
+const allResponses: string[] = [...pResponses, ...nResponses, "Maybe", "Possibly", "Perhaps"];
---
+++ src/commands/qrcode.ts
@@ -0,0 +1,24 @@
+import { ChatInputCommand } from "#util";
+import { MessageEmbed } from "discord.js";
+import * as QRCode from "qrcode";
+
+export const qrcode: ChatInputCommand = {
+    name: "qrcode",
+    description: "Generate a QR-code",
+    options: [
+        {
+            name: "url",
+            description: "The URL (or text) to store in the QR code",
+            type: "STRING",
+            required: true,
+        },
+    ],
+    run: async (interaction) => {
+        const input = interaction.options.get("url").value.toString();
+
+        await QRCode.toFile("./qrcode.png", input);
+
+        const embed = new MessageEmbed({ title: "test", image: { url: "attachment://qrcode.png" } });
+        await interaction.reply({ embeds: [embed] });
+    },
+};
