dece04e3875b943678af4c693e713b8d343bb1f3
Josey C. Wörmann <hi@josey.dev>
2023-12-20 01:12:37 +0100

    fix

--- src/util/modules/Client.ts
+++ src/util/modules/Client.ts
@@ -1,15 +1,8 @@
 import { Client as DiscordClient, Partials } from "discord.js";
-// import { Client as StatcordClient } from "statcord.js";
-import { intents, statcordToken } from "#util";
+import { intents } from "#util";
 
 export class CustomClient extends DiscordClient {
-    /**
-     * The Statcord-client for metrics
-     */
-    // metrics: StatcordClient;
-
     constructor() {
         super({ intents: intents, partials: [Partials.Message, Partials.Reaction] });
-        // this.metrics = new StatcordClient({ client: this, key: statcordToken });
     }
 }
